4bed8b7cdea05e25985fdac116c00e3c4c0447a7
DonyAgadony <162630322+DonyAgadony@users.noreply.github.com>
2024-11-22 10:09:06 +0200

    Finished The Question

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿class Member
+{
+    string Name
+    { get; set; }
+    public bool IsCoal
+    { get; set; }
+
+    public Member(string Name, bool IsCoal)
+    {
+        this.Name = Name;
+        this.IsCoal = IsCoal;
+    }
+}
+class Committee
+{
+    string name { get; set; }
+    Member[] members { get; set; }
+    public int count { get; set; }
+    public Committee(string name, Member[] members, int count)
+    {
+        this.name = name;
+        this.members = members;
+        this.count = count;
+    }
+
+    public int Count(bool belong)
+    {
+        int sum = 0;
+        for (int i = 0; i < members.Length; i++)
+        {
+            if (members[i].IsCoal == belong) { sum++; }
+        }
+        return sum;
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+
+    }
+
+    public static int Amount(Committee[] arr, Member m)
+    {
+        int sum = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (m.IsCoal)
+            {
+                if (arr[i].Count(true) + 1 > arr[i].Count(false) && arr[i].count + 1 < 16)
+                {
+                    sum++;
+                }
+            }
+            else if (arr[i].Count(true) > arr[i].Count(false) + 1 && arr[i].count + 1 < 16)
+            {
+                sum++;
+            }
+        }
+        return sum;
+    }
+}
